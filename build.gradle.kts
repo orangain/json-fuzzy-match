6237a34f53cad6ffc18581164a38a123c1533f7f
orangain <orangain@gmail.com>
2019-09-25 21:02:49 +0900

    Add build config to upload to bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    java
+    `java-library`
     `maven-publish`
     kotlin("jvm") version "1.3.50"
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 group = "com.capybala.jsonmatch"
@@ -29,10 +30,66 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc)
+}
+
 publishing {
     publications {
-        create<MavenPublication>("maven") {
+        register<MavenPublication>("maven") {
             from(components["java"])
+            artifact(sourcesJar.get())
+            artifact(javadocJar.get())
+            groupId = project.group as String
+            artifactId = project.name
+            version = project.version as String
+            pom {
+                name.set(project.name)
+                description.set("Custom assertion to check JSON string matches pattern.")
+                url.set("https://github.com/orangain/jsonmatch")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/orangain/jsonmatch/blob/master/LICENSE")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("orangain")
+                        name.set("Kota Kato")
+                        email.set("orangain@gmail.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/orangain/jsonmatch")
+                }
+            }
         }
     }
+}
+
+bintray {
+    val bintrayUser: String by project
+    val bintrayAPIKey: String by project
+    user = bintrayUser
+    key = bintrayAPIKey
+    setPublications("maven")
+    with(pkg) {
+        repo = "maven"
+        name = "jsonmatch"
+        with(version) {
+            name = project.version as String
+        }
+    }
+}
+
+tasks.bintrayUpload {
+    dependsOn(tasks.build)
 }
