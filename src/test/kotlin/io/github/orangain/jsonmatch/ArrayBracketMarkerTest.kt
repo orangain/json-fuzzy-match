46f3e659d533456279f27558643f125aabde0566
orangain <orangain@gmail.com>
2020-07-19 18:01:50 +0900

    Add tests for array markers

---
+++ src/test/kotlin/io/github/orangain/jsonmatch/ArrayBracketMarkerTest.kt
@@ -0,0 +1,74 @@
+package io.github.orangain.jsonmatch
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class ArrayBracketMarkerTest {
+    private val patternJson = """{ "a": "#[]" }"""
+
+    @Test
+    fun doesNotMatchMissingField() {
+        Assertions.assertThatThrownBy {
+            // language=JSON
+            JsonStringAssert.assertThat("{}").jsonMatches(patternJson)
+        }.isInstanceOf(AssertionError::class.java)
+            .hasMessageContaining("path: \$, actual: {}, expected: {a=#[]}, reason: all key-values did not match, expected has un-matched keys: [a]")
+    }
+
+    @Test
+    fun doesNotMatchNullField() {
+        Assertions.assertThatThrownBy {
+            // language=JSON
+            JsonStringAssert.assertThat("""{ "a": null }""").jsonMatches(patternJson)
+        }.isInstanceOf(AssertionError::class.java)
+            .hasMessageContaining("path: $.a, actual: null, expected: '#[]', reason: not an array or list")
+    }
+
+    @Test
+    fun doesNotMatchBoolean() {
+        Assertions.assertThatThrownBy {
+            // language=JSON
+            JsonStringAssert.assertThat("""{ "a": true }""").jsonMatches(patternJson)
+        }.isInstanceOf(AssertionError::class.java)
+            .hasMessageContaining("path: $.a, actual: true, expected: '#[]', reason: not an array or list")
+    }
+
+    @Test
+    fun doesNotMatchNumber() {
+        Assertions.assertThatThrownBy {
+            // language=JSON
+            JsonStringAssert.assertThat("""{ "a": 1 }""").jsonMatches(patternJson)
+        }.isInstanceOf(AssertionError::class.java)
+            .hasMessageContaining("path: $.a, actual: 1, expected: '#[]', reason: not an array or list")
+    }
+
+    @Test
+    fun doesNotMatchString() {
+        Assertions.assertThatThrownBy {
+            // language=JSON
+            JsonStringAssert.assertThat("""{ "a": "true" }""").jsonMatches(patternJson)
+        }.isInstanceOf(AssertionError::class.java)
+            .hasMessageContaining("path: $.a, actual: 'true', expected: '#[]', reason: not an array or list")
+    }
+
+    @Test
+    fun matchArray() {
+        // language=JSON
+        JsonStringAssert.assertThat("""{ "a": [1, 2, 3] }""").jsonMatches(patternJson)
+    }
+
+    @Test
+    fun matchEmptyArray() {
+        // language=JSON
+        JsonStringAssert.assertThat("""{ "a": [] }""").jsonMatches(patternJson)
+    }
+
+    @Test
+    fun doesNotMatchObject() {
+        Assertions.assertThatThrownBy {
+            // language=JSON
+            JsonStringAssert.assertThat("""{ "a": {} }""").jsonMatches(patternJson)
+        }.isInstanceOf(AssertionError::class.java)
+            .hasMessageContaining("path: $.a, actual: {}, expected: '#[]', reason: not an array or list")
+    }
+}
---
+++ src/test/kotlin/io/github/orangain/jsonmatch/ArrayBracketMarkerWithNestedChildrenTest.kt
@@ -0,0 +1,44 @@
+package io.github.orangain.jsonmatch
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class ArrayBracketMarkerWithNestedChildrenTest {
+    private val patternJson = """{ "a": "#[] #[] #number" }"""
+
+    @Test
+    fun matchNestedNumberArray() {
+        // language=JSON
+        JsonStringAssert.assertThat("""{ "a": [[1, 2, 3], [4, 5, 6]] }""").jsonMatches(patternJson)
+    }
+
+    @Test
+    fun matchEmptyArray() {
+        // language=JSON
+        JsonStringAssert.assertThat("""{ "a": [] }""").jsonMatches(patternJson)
+    }
+
+    @Test
+    fun doesNotMatchFlatNumberArray() {
+        Assertions.assertThatThrownBy {
+            // language=JSON
+            JsonStringAssert.assertThat("""{ "a": [1, 2, 3] }""").jsonMatches(patternJson)
+        }.isInstanceOf(AssertionError::class.java)
+            .hasMessageContaining("path: $[0], actual: 1, expected: '#[] #number', reason: not an array or list")
+    }
+
+    @Test
+    fun matchNestedEmptyArray() {
+        // language=JSON
+        JsonStringAssert.assertThat("""{ "a": [[]] }""").jsonMatches(patternJson)
+    }
+
+    @Test
+    fun doesNotMatchNestedArrayContainsString() {
+        Assertions.assertThatThrownBy {
+            // language=JSON
+            JsonStringAssert.assertThat("""{ "a": [["1", "2", "3"]] }""").jsonMatches(patternJson)
+        }.isInstanceOf(AssertionError::class.java)
+            .hasMessageContaining("path: $[0], actual: '1', expected: '#number', reason: not a number")
+    }
+}
