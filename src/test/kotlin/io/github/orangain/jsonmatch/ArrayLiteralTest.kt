511a7b6756535e13312daf6186728f2b1ebd3de6
orangain <orangain@gmail.com>
2020-07-19 20:20:16 +0900

    Add tests for array and object literal

---
+++ src/test/kotlin/io/github/orangain/jsonmatch/ArrayLiteralTest.kt
@@ -0,0 +1,64 @@
+package io.github.orangain.jsonmatch
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class ArrayLiteralTest {
+
+    // language=JSON
+    val json = """
+        [
+            {"id": 1},
+            {"id": 2},
+            {"id": 3}
+        ]
+    """.trimIndent()
+
+    @Test
+    fun matchArray() {
+        // language=JSON
+        JsonStringAssert.assertThat(json).jsonMatches(
+            """
+                [
+                    {"id": 1},
+                    {"id": 2},
+                    {"id": 3}
+                ]
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun doesNotMatchLongerArray() {
+        Assertions.assertThatThrownBy {
+            // language=JSON
+            JsonStringAssert.assertThat(json).jsonMatches(
+                """
+                [
+                    {"id": 1},
+                    {"id": 2},
+                    {"id": 3},
+                    {"id": 4}
+                ]
+            """.trimIndent()
+            )
+        }.isInstanceOf(AssertionError::class.java)
+            .hasMessageContaining("""path: ${'$'}, actual: [{"id":1},{"id":2},{"id":3}], expected: [{"id":1},{"id":2},{"id":3},{"id":4}], reason: actual and expected arrays are not the same size - 3:4""")
+    }
+
+    @Test
+    fun doesNotMatchShorterArray() {
+        Assertions.assertThatThrownBy {
+            // language=JSON
+            JsonStringAssert.assertThat(json).jsonMatches(
+                """
+                [
+                    {"id": 1},
+                    {"id": 2}
+                ]
+            """.trimIndent()
+            )
+        }.isInstanceOf(AssertionError::class.java)
+            .hasMessageContaining("""path: ${'$'}, actual: [{"id":1},{"id":2},{"id":3}], expected: [{"id":1},{"id":2}], reason: actual and expected arrays are not the same size - 3:2""")
+    }
+}
---
+++ src/test/kotlin/io/github/orangain/jsonmatch/ObjectLiteralTest.kt
@@ -0,0 +1,42 @@
+package io.github.orangain.jsonmatch
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class ObjectLiteralTest {
+
+    // language=JSON
+    val json = """{ "a": 1, "b": true, "c": null }"""
+
+    @Test
+    fun matchObject() {
+        // language=JSON
+        JsonStringAssert.assertThat(json).jsonMatches("""{ "a": 1, "b": true, "c": null }""")
+    }
+
+    @Test
+    fun matchObjectWithoutPreservingOrder() {
+        // language=JSON
+        JsonStringAssert.assertThat(json).jsonMatches("""{ "c": null, "b": true, "a": 1 }""")
+    }
+
+    @Test
+    fun doesNotMatchLargerObject() {
+        Assertions.assertThatThrownBy {
+            // language=JSON
+            JsonStringAssert.assertThat(json).jsonMatches(
+                """{ "a": 1, "b": true, "c": null, "d": "test", "e": {} }"""
+            )
+        }.isInstanceOf(AssertionError::class.java)
+            .hasMessageContaining("""path: ${'$'}, actual: {"a":1,"b":true,"c":null}, expected: {a=1, b=true, c=null, d=test, e={}}, reason: all key-values did not match, expected has un-matched keys: [d, e]""")
+    }
+
+    @Test
+    fun doesNotMatchSmallerObject() {
+        Assertions.assertThatThrownBy {
+            // language=JSON
+            JsonStringAssert.assertThat(json).jsonMatches("""{ "a": 1 }""")
+        }.isInstanceOf(AssertionError::class.java)
+            .hasMessageContaining("""path: ${'$'}, actual: {"a":1,"b":true,"c":null}, expected: {a=1}, reason: actual value has 2 more key(s) than expected: {b=true, c=null}""")
+    }
+}
