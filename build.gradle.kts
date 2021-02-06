a92d19c9d75686dfe9aeabe846786e8b857d5d6b
orangain <orangain@gmail.com>
2021-02-06 13:50:31 +0900

    Remove bintray-related settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     `java-library`
     `maven-publish`
     kotlin("jvm") version "1.3.50"
-    id("com.jfrog.bintray") version "1.8.4"
 }
 
 group = "io.github.orangain.json-fuzzy-match"
@@ -38,58 +37,4 @@ val sourcesJar by tasks.registering(Jar::class) {
 val javadocJar by tasks.registering(Jar::class) {
     archiveClassifier.set("javadoc")
     from(tasks.javadoc)
-}
-
-publishing {
-    publications {
-        register<MavenPublication>("maven") {
-            from(components["java"])
-            artifact(sourcesJar.get())
-            artifact(javadocJar.get())
-            groupId = project.group as String
-            artifactId = project.name
-            version = project.version as String
-            pom {
-                name.set(project.name)
-                description.set("Custom assertion to check JSON string matches pattern.")
-                url.set("https://github.com/orangain/json-fuzzy-match")
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://github.com/orangain/json-fuzzy-match/blob/master/LICENSE")
-                        distribution.set("repo")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("orangain")
-                        name.set("Kota Kato")
-                        email.set("orangain@gmail.com")
-                    }
-                }
-                scm {
-                    url.set("https://github.com/orangain/json-fuzzy-match")
-                }
-            }
-        }
-    }
-}
-
-bintray {
-    val bintrayUser: String? by project
-    val bintrayAPIKey: String? by project
-    user = bintrayUser
-    key = bintrayAPIKey
-    setPublications("maven")
-    with(pkg) {
-        repo = "maven"
-        name = "json-fuzzy-match"
-        with(version) {
-            name = project.version as String
-        }
-    }
-}
-
-tasks.bintrayUpload {
-    dependsOn(tasks.build)
 }
